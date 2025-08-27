d2c0c374a1b4b5f8f78e5d4a5eef1d5a2011c4d9
Ricks <bricks@unomaha.edu>
2025-08-27 15:14:08 -0500

    Add grayscale image conversion script

---
+++ 01-grayscale-image.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+image = Image.open("./horse.jpg")
+data = image.load()
+
+for x in range(image.width):
+  for y in range(image.height):
+    pixel = data[x,y]
+
+    r = pixel[0]
+    g = pixel[1]
+    b = pixel[2]
+
+    k = .33 * r + .33*g + .33*b
+    k = int(k)
+
+    data[x,y] = (k, k, k)
+
+image.save("./horse.png")
